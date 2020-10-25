34e599b7ca838778b19fa0954694272ea7b699c2
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-10-25 13:19:23 -0300

    feat: developing the migrations of the application

---
+++ src/shared/infra/typeorm/migrations/1603588283647-CreateUsers.ts
@@ -0,0 +1,65 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUsers1603588283647 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: 'university',
+            type: 'varchar',
+          },
+          {
+            name: 'registration',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password_hash',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
---
+++ src/shared/infra/typeorm/migrations/1603596379484-CreateStudents.ts
@@ -0,0 +1,71 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateStudents1603596379484 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'students',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'university',
+            type: 'varchar',
+          },
+          {
+            name: 'registration',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'is_advisor',
+            type: 'varchar',
+            isNullable: false,
+            default: false,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'User_FK',
+            columnNames: ['user_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('students', 'User_FK');
+
+    await queryRunner.dropTable('students');
+  }
+}
