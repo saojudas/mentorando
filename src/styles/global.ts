0fa9ae27ae9d7f8921092bb248fb905d1d4bd51a
Bruno Futema <bruno.futema@outlook.com>
2020-10-25 19:16:35 -0300

    style: Add global style

---
+++ src/styles/global.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased !important;
+    background: ${(props) => props.theme.colors.background};
+  }
+
+  body, input, textarea, button {
+    font: 14px Poppins, Archivo, sans-serif;
+  }
+
+  a {
+    text-decoration: none !important;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+`;
