c4e732c2bba000198b9e878107b683e73d974776
Bruno Futema <bruno.futema@outlook.com>
2020-10-25 19:14:30 -0300

    style: Add light theme config

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,28 @@
+export default {
+  title: 'light',
+  colors: {
+    primaryLighter: '#B6B6F2',
+    primaryLight: '#9595E9',
+    primary: '#7475FF',
+
+    secondaryLighter: '#87F8C9',
+    secondaryLight: '#4FF1AE',
+    secondary: '#2AF39F',
+
+    primaryGradient: 'linear-gradient(90deg, #7475FF 0%, #9595E9 100%);',
+    secondaryGradient: 'linear-gradient(90deg, #2AF39F 0%, #87F8C9 100%);',
+
+    white: '#FBFBFB',
+    black: '#313131',
+    blue: '#12D8FA',
+    red: '#E0514F',
+    orange: '#FF7A00',
+
+    grayLight: '#909090',
+    gray: '#808080',
+    grayDark: '#707070',
+
+    background: '#FBFBFB',
+    text: '#313131',
+  },
+};
