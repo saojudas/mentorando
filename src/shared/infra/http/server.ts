fbdd91cf23136ff3a443960fcf80551ff204b69d
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-10-25 13:20:23 -0300

    feat: developing the routes of the application

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,34 @@
+import 'reflect-metadata';
+
+import express, { NextFunction, Request, Response } from 'express';
+
+import 'express-async-errors';
+
+import Exceptions from '@shared/errors/Exceptions';
+import routes from './routes';
+
+import '@shared/infra/typeorm';
+import '@shared/container';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof Exceptions) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('Server is running on port 3333!');
+});
