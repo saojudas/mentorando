bdd245a907761b21d97e6807f3feba3778f1bc32
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-10-25 13:30:04 -0300

    feat: developing he teacher repository

---
+++ src/modules/teachers/infra/typeorm/repositories/TeachersRepository.ts
@@ -0,0 +1,36 @@
+import { getRepository, Repository } from 'typeorm';
+
+import ICreateTeacherDTO from '@modules/teachers/dtos/ICreateTeacherDTO';
+import ITeachersRepository from '@modules/teachers/repositories/ITeachersRepository';
+
+import Teacher from '../entities/Teacher';
+
+class TeachersRepository implements ITeachersRepository {
+  private ormRepository: Repository<Teacher>;
+
+  constructor() {
+    this.ormRepository = getRepository(Teacher);
+  }
+
+  public async create(data: ICreateTeacherDTO): Promise<Teacher> {
+    const teacher = this.ormRepository.create(data);
+
+    await this.ormRepository.save(teacher);
+
+    return teacher;
+  }
+
+  public async save(teacher: Teacher): Promise<Teacher> {
+    await this.ormRepository.save(teacher);
+
+    return teacher;
+  }
+
+  public async findByName(name: string): Promise<Teacher | undefined> {
+    const teacher = await this.ormRepository.findOne({ where: { name } });
+
+    return teacher;
+  }
+}
+
+export default TeachersRepository;
