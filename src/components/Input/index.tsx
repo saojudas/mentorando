86fe91347ed8d76a1c61074fbac427d2a39bb72c
Bruno Futema <bruno.futema@outlook.com>
2020-10-28 02:32:56 -0300

    feat: Add hover color change for ux visibility

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,9 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
 
 import { Container } from './styles';
@@ -8,11 +13,33 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    <input {...rest} />
-    {Icon && <Icon size={20} />}
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  return (
+    <Container isFocused={isFocused} isFilled={isFilled}>
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        ref={inputRef}
+        {...rest}
+      />
+      {Icon && <Icon size={20} />}
+    </Container>
+  );
+};
 
 export default Input;
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,12 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
-  background: ${(props) => props.theme.colors.white};
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: ${props => props.theme.colors.white};
   border-radius: 4px;
-  border: 2px solid ${(props) => props.theme.colors.primaryLighter};
+  border: 2px solid ${props => props.theme.colors.primaryLighter};
   padding: 8px 16px;
   width: 100%;
-  color: ${(props) => props.theme.colors.grayLight};
+  color: ${props => props.theme.colors.grayLight};
 
   display: flex;
   align-items: center;
@@ -16,14 +21,27 @@ export const Container = styled.div`
     margin-top: 32px;
   }
 
+  ${props =>
+    props.isFocused &&
+    css`
+      color: ${props.theme.colors.primary};
+      border-color: ${props.theme.colors.primary};
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: ${props.theme.colors.primary};
+    `}
+
   input {
     background: transparent;
     flex: 1;
     border: 0;
-    color: ${(props) => props.theme.colors.black};
+    color: ${props => props.theme.colors.black};
 
     &::placeholder {
-      color: ${(props) => props.theme.colors.grayLight};
+      color: ${props => props.theme.colors.grayLight};
     }
   }
 
