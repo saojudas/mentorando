b7857deb23e72732dc22bf70708dd98187f519a5
Bruno Futema <bruno.futema@outlook.com>
2020-10-29 23:25:23 -0300

    feat: Add Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CardVideo from '../../components/CardVideo';
+
+import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <CardVideo />
+    </Container>
+  );
+};
+
+export default Home;
