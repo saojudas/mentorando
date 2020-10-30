5e1c261a2a12f76f4114ec50edfddd8fe349a935
Bruno Futema <bruno.futema@outlook.com>
2020-10-29 23:26:01 -0300

    feat: Add Home page route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
+import Home from '../pages/Home';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={SignIn} />
+      <Route path="/home" component={Home} />
     </Switch>
   </BrowserRouter>
 );
