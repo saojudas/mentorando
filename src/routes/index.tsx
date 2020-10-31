e93282949c3471005096e3e54e0a3ca48d96e444
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-10-30 23:57:14 -0300

    feat: including the routes signin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Header from '../components/Header';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
+      <Route path="/header" component={Header} />
       <Route exact path="/" component={SignIn} />
+      <Route path="/register" component={SignUp} />
     </Switch>
   </BrowserRouter>
 );
