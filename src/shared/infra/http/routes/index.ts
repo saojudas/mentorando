7b7ca06ffd72e52c07a680031e41088bf4ac42db
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-01 22:29:03 -0300

    feat: adding the tags routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/session.routes';
 import areasRouter from '@modules/teachers/infra/http/routes/areas.routes';
+import tagsRouter from '@modules/videos/infra/http/routes/tags.routes';
 
 const routes = Router();
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/areas', areasRouter);
+routes.use('/tags', tagsRouter);
 
 export default routes;
