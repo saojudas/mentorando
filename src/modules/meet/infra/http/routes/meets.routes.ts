05aa69bd8b13d141ca73391cbd2afa6a15bd38d3
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-02 02:30:43 -0300

    feat: adding the routes to the application

---
+++ src/modules/meet/infra/http/routes/meets.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
+import MeetsController from '../controllers/MeetsController';
+
+const meetRouter = Router();
+
+const meetsController = new MeetsController();
+
+meetRouter.use(ensureAuthenticated);
+
+meetRouter.get('/', meetsController.index);
+meetRouter.post('/', meetsController.create);
+
+export default meetRouter;
