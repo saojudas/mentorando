8eb1b94e574562c83e624c845d529a7719da37f1
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-01 22:27:17 -0300

    feat: adding the Tag container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -12,6 +12,9 @@ import StudentsRepository from '@modules/students/infra/typeorm/repositories/Stu
 import IAreasRepository from '@modules/teachers/repositories/IAreasRepository';
 import AreasRepository from '@modules/teachers/infra/typeorm/repositories/AreasRepository';
 
+import ITagsRepository from '@modules/videos/repositories/ITagsRepository';
+import TagsRepository from '@modules/videos/infra/typeorm/repositories/TagsRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
@@ -31,3 +34,5 @@ container.registerSingleton<IAreasRepository>(
   'AreasRepository',
   AreasRepository,
 );
+
+container.registerSingleton<ITagsRepository>('TagsRepository', TagsRepository);
