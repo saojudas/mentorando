2714b55ff6677a30905f7bc40e72f734f12f30a7
Bruno Futema <bruno.futema@outlook.com>
2020-11-10 01:51:45 -0300

    feat: :building_construction: Add root saga to combine sagas

---
+++ src/store/modules/rootSaga.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { all } from 'redux-saga/effects';
+
+export default function* rootSaga() {
+  return yield all([]);
+}
