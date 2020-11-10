d4503271c0313c24956d86c6233bc556c064d0d2
Bruno Futema <bruno.futema@outlook.com>
2020-11-10 01:53:30 -0300

    feat: :building_construction: Add persist reducer to persist reducers

---
+++ src/store/persistReducers.ts
@@ -0,0 +1,16 @@
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { AnyAction, Reducer } from 'redux';
+
+export default (reducers: Reducer): Reducer<any, AnyAction> => {
+  const persistedReducer = persistReducer(
+    {
+      key: '@MENTORANDO',
+      storage,
+      whitelist: [],
+    },
+    reducers,
+  );
+
+  return persistedReducer;
+};
