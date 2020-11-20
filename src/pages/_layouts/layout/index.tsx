aa346d4f6b45751615758e0bd502477f21d1208b
Bruno Futema <bruno.futema@outlook.com>
2020-11-20 01:14:18 -0300

    feat: :sparkles: Add Header component to private pages

--- src/pages/_layouts/layout/index.tsx
+++ src/pages/_layouts/layout/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Header from '../../../components/Header';
+
 import { Wrapper, Content } from './styles';
 
 const DefaultLayout: React.FC = ({ children }) => (
   <Wrapper>
+    <Header />
     <Content>{children}</Content>
   </Wrapper>
 );
--- src/pages/_layouts/layout/styles.ts
+++ src/pages/_layouts/layout/styles.ts
@@ -2,4 +2,6 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div``;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  padding: 20px 60px 20px;
+`;
