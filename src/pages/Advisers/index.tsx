d8511735126b85a23d360d90b9ac800edf485964
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-20 01:16:57 -0300

    feat: creating the file Page Adviser

---
+++ src/pages/Advisers/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardUser from '../../components/CardUser';
+
+import { Container } from './styles';
+
+const Advisers: React.FC = () => {
+  return (
+    <Container>
+      <CardUser />
+    </Container>
+  );
+};
+
+export default Advisers;
