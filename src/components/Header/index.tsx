7c5d1cda8a55c94d00659c38e328ccc6d294a3e6
Bruno Futema <bruno.futema@outlook.com>
2020-11-20 01:09:01 -0300

    feat: :sparkles: Connect Header component to user reducer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,28 @@
 import React, { useContext } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { ThemeContext } from 'styled-components';
-import { Container, Logo, NavigationBar, Profile } from './styles';
 
 import logo from '../../assets/logo.svg';
 import userImg from '../../assets/jhon.jpg';
 
+import { IRootState } from '../../store';
+
+import { Container, Logo, NavigationBar, Profile } from './styles';
+
 const Header: React.FC = () => {
   const { colors } = useContext(ThemeContext);
+
+  const profile = useSelector((state: IRootState) => state.user.profile);
+
   return (
     <Container color={colors.primary}>
       <Logo>
-        <img src={logo} alt="Mentorando" />
+        <Link to="/home">
+          <img src={logo} alt="Mentorando" />
+        </Link>
       </Logo>
+
       <NavigationBar color={colors.white}>
         <ul>
           <li>
@@ -26,8 +36,10 @@ const Header: React.FC = () => {
           </li>
         </ul>
       </NavigationBar>
+
       <Profile color={colors.white}>
-        <span>Jhonatan da Costa</span>
+        <span>{profile?.username}</span>
+
         <img src={userImg} alt="Jhonatan" />
       </Profile>
     </Container>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,24 +6,41 @@ interface ColorProps {
 
 export const Container = styled.header<ColorProps>`
   background: ${props => props.color};
-  height: 70px;
+
+  height: 80px;
   width: 100%;
+
+  padding: 48px 60px;
+
   display: flex;
   align-items: center;
-  padding: 0 60px;
   justify-content: space-between;
 `;
 
-export const Logo = styled.div``;
+export const Logo = styled.div`
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(90%);
+  }
+`;
 
 export const NavigationBar = styled.nav<ColorProps>`
   ul {
     display: flex;
+
     li {
       margin: 0 15px;
+
       a {
-        font-weight: bold;
         color: ${props => props.color};
+        font-size: 18px;
+        font-weight: bold;
+
+        &:hover {
+          color: ${props => props.theme.colors.secondary};
+          border-bottom: 2px solid ${props => props.theme.colors.white};
+        }
       }
     }
   }
@@ -33,21 +50,31 @@ export const Profile = styled.div<ColorProps>`
   display: flex;
   align-items: center;
 
+  &:hover {
+    filter: brightness(90%);
+  }
+
   span {
+    color: ${props => props.color};
     font-size: 1.3em;
     font-weight: bold;
-    color: ${props => props.color};
-    cursor: pointer;
-    min-width: 120px;
     text-align: right;
     white-space: nowrap;
+
+    min-width: 120px;
+
+    cursor: pointer;
   }
+
   img {
     width: 45px;
     height: 45px;
-    object-fit: contain;
-    border-radius: 50%;
-    margin-left: 20px;
+
     border: 1px solid ${props => props.color};
+    border-radius: 50%;
+
+    object-fit: contain;
+
+    margin-left: 20px;
   }
 `;
