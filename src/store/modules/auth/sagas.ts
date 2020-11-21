615b0ce7365593fbf384d2d465c3589f65ac82c9
Bruno Futema <bruno.futema@outlook.com>
2020-11-21 00:29:04 -0300

    feat: :sparkles: Integrating SignUp page to api

--- src/store/modules/auth/sagas.ts
+++ src/store/modules/auth/sagas.ts
@@ -1,12 +1,19 @@
 import { all, takeLatest, call, put, delay } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import { AxiosResponse } from 'axios';
 import { AnyAction } from 'redux';
 
 import api from '../../../services/api';
 import history from '../../../services/history';
 
-import { signInSuccess, signInFailure } from './actions';
-import { SIGN_IN_REQUEST } from './constants';
+import {
+  signInSuccess,
+  signInFailure,
+  signUpSuccess,
+  signUpFailure,
+} from './actions';
+
+import { SIGN_IN_REQUEST, SIGN_UP_REQUEST } from './constants';
 
 export function* signIn({ payload }: AnyAction) {
   try {
@@ -25,10 +32,53 @@ export function* signIn({ payload }: AnyAction) {
 
     yield put(signInSuccess(token, user));
 
+    toast.success('Bem vindo, login efetuado com sucesso!');
+
     history.push('/home');
   } catch (err) {
     yield put(signInFailure());
+
+    toast.error('Login não efetuado, verifique suas credenciais!');
   }
 }
 
-export default all([takeLatest(SIGN_IN_REQUEST, signIn)]);
+export function* signUp({ payload }: AnyAction) {
+  try {
+    const {
+      username,
+      name,
+      email,
+      university,
+      registration,
+      password,
+      confirm_password,
+      is_student,
+    } = payload;
+
+    yield call(api.post, 'users', {
+      username,
+      name,
+      email,
+      university,
+      registration,
+      password,
+      confirm_password,
+      is_student: is_student[0] === 'true',
+    });
+
+    yield delay(1000);
+
+    toast.success('Cadastro efetuado com sucesso!');
+
+    yield put(signUpSuccess());
+  } catch (err) {
+    yield put(signUpFailure());
+
+    toast.error('Cadastro não efetuado, verifique seus dados!');
+  }
+}
+
+export default all([
+  takeLatest(SIGN_IN_REQUEST, signIn),
+  takeLatest(SIGN_UP_REQUEST, signUp),
+]);
