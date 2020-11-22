07706532b44e4ecb9069cc13e3682fc43a156474
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-22 18:20:06 -0300

    feat: developing the page Video

--- src/pages/Video/index.tsx
+++ src/pages/Video/index.tsx
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+
+import Iframe from 'react-iframe';
 
 import AsideMenu from '../../components/AsideMenu';
+import Tag from '../../components/Tag';
 
-import { Container, Title, ContentSection, Content } from './styles';
+import {
+  Container,
+  Title,
+  ContentSection,
+  Content,
+  FrameVideo,
+  Description,
+  Tags,
+} from './styles';
 
 const Video: React.FC = () => {
+  const [isTheater, setIsTheater] = useState<boolean>(true);
+
+  const toggleTheater = useCallback(() => {
+    setIsTheater(!isTheater);
+  }, [isTheater]);
+
   return (
     <Container>
-      <Title>
-        <h1>Início</h1>
-        <section>teste</section>
-      </Title>
       <ContentSection>
-        <AsideMenu />
-        <Content />
+        {isTheater && <AsideMenu />}
+        <Content isTheater={isTheater}>
+          {isTheater && (
+            <Title>
+              <h1>
+                O último Code/Drops! Script que calcula horas de vídeo |
+                Code/Drops #68
+              </h1>
+            </Title>
+          )}
+          <FrameVideo>
+            <button type="button" onClick={toggleTheater}>
+              MODO TEATRO
+            </button>
+            <Iframe
+              url="https://www.youtube.com/embed/faekjlZuTFA"
+              width="100%"
+              height="664px"
+              allow="fullscreen"
+            />
+          </FrameVideo>
+          <Description>
+            <h3>Descrição</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              consequuntur dolores accusamus voluptate. Iure, a, blanditiis
+              dolore sapiente ipsa iste numquam, quisquam non neque corporis
+              quidem sunt quis expedita delectus.
+            </p>
+          </Description>
+          <Tags>
+            <Tag name="React JS" />
+            <Tag name="Node JS" />
+            <Tag name="Docker" />
+          </Tags>
+        </Content>
       </ContentSection>
     </Container>
   );
