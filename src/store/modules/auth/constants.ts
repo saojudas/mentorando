ff9b0f9a4c07a2d2e88a555e91e9054aa0c5b513
Bruno Futema <bruno.futema@outlook.com>
2020-11-22 03:27:05 -0300

    feat: :sparkles: Add sign out type for redux

--- src/store/modules/auth/constants.ts
+++ src/store/modules/auth/constants.ts
@@ -5,3 +5,5 @@ export const SIGN_IN_FAILURE = '@auth/SIGN_IN_FAILURE';
 export const SIGN_UP_REQUEST = '@auth/SIGN_UP_REQUEST';
 export const SIGN_UP_SUCCESS = '@auth/SIGN_UP_SUCCESS';
 export const SIGN_UP_FAILURE = '@auth/SIGN_UP_FAILURE';
+
+export const SIGN_OUT = '@auth/SIGN_OUT';
