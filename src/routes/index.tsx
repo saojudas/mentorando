713c3cd9a0ebd92f3fe92264a47c8564fdebc2aa
Bruno Futema <bruno.futema@outlook.com>
2020-11-22 03:34:10 -0300

    feat: :sparkles: Add more routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,15 @@ import Route from './Route';
 import Landing from '../pages/Landing';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+
 import Home from '../pages/Home';
+
+import NewVideo from '../pages/NewVideo';
 import NewMeet from '../pages/NewMeet';
-import Report from '../pages/Report';
+import NewReport from '../pages/NewReport';
+
+import Advisors from '../pages/Advisors';
+import Reports from '../pages/Reports';
 
 import history from '../services/history';
 
@@ -17,9 +23,15 @@ const Routes: React.FC = () => (
       <Route exact path="/" component={Landing} landing />
       <Route path="/login" component={SignIn} />
       <Route path="/register" component={SignUp} />
+
       <Route path="/home" component={Home} isPrivate />
-      <Route path="/meet" component={NewMeet} isPrivate />
-      <Route path="/report" component={Report} isPrivate />
+
+      <Route path="/new-video" component={NewVideo} isPrivate />
+      <Route path="/new-meet" component={NewMeet} isPrivate />
+      <Route path="/new-report" component={NewReport} isPrivate />
+
+      <Route path="/advisors" component={Advisors} isPrivate />
+      <Route path="/reports" component={Reports} isPrivate />
     </Switch>
   </Router>
 );
