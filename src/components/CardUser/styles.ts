ce370c0db7b3b8d93ce05abd7a55b524a8ce2852
Bruno Futema <bruno.futema@outlook.com>
2020-11-22 15:51:23 -0300

    feat: :sparkles: Add link to user example

--- src/components/CardUser/styles.ts
+++ src/components/CardUser/styles.ts
@@ -6,11 +6,10 @@ interface ColorProps {
 
 export const Container = styled.div`
   width: 180px;
-  height: 180px;
   position: relative;
 
   border-radius: 8px;
-  border: 1.5px solid ${props => props.theme.colors.primaryLight} !important;
+  border: 1px solid ${props => props.theme.colors.primaryLight} !important;
   box-shadow: inset 0 0 1em transparent,
     0 0 0.3em ${props => props.theme.colors.primary};
 
@@ -23,6 +22,7 @@ export const Container = styled.div`
 
 export const CardBody = styled.div`
   min-height: 139px;
+
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -32,22 +32,27 @@ export const CardBody = styled.div`
   img {
     height: 76px;
     width: 76px;
+
     object-fit: contain;
+
     border-radius: 50%;
     margin-bottom: 9px;
   }
 `;
 
 export const TagArea = styled.div<ColorProps>`
-  background-color: ${props => props.color};
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  align-items: center;
-  border-radius: 8px;
-  border: 1px solid ${props => props.theme.colors.secondary};
   width: fit-content;
   padding: 0 10px;
+
+  text-align: center;
+
+  border-radius: 8px;
+  border: 1px solid ${props => props.theme.colors.secondary};
+  background-color: ${props => props.color};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   span {
     text-transform: uppercase;
@@ -56,13 +61,15 @@ export const TagArea = styled.div<ColorProps>`
 `;
 
 export const CardInfo = styled.div<ColorProps>`
-  background-color: ${props => props.color};
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  align-items: center;
   height: 38px;
+
+  text-align: center;
+
   border-radius: 0 0 7px 7px;
-  border: 1px solid ${props => props.theme.colors.primaryLight};
-  border-top: 2px solid ${props => props.theme.colors.primaryLight};
+  border-top: 1px solid ${props => props.theme.colors.primaryLight};
+  background-color: ${props => props.color};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
