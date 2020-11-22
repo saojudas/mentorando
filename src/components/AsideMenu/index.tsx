639f4cfecff1ad51d9a0f06a2a2bbcf419347fb5
Bruno Futema <bruno.futema@outlook.com>
2020-11-22 03:24:21 -0300

    feat: :art: Add animation in aside menu component

--- src/components/AsideMenu/index.tsx
+++ src/components/AsideMenu/index.tsx
@@ -1,16 +1,83 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  FaPlus,
+  FaRegPlayCircle,
+  FaRegCalendarPlus,
+  FaUsers,
+  FaArchive,
+} from 'react-icons/fa';
+import { ThemeContext } from 'styled-components';
+import Ink from 'react-ink';
 
-import { Container, Item } from './styles';
+import advisorImg from '../../assets/advisor-light.svg';
+
+import { Container, Item, Animation } from './styles';
 
 const AsideMenu: React.FC = () => {
+  const { colors } = useContext(ThemeContext);
+
   return (
     <Container>
-      <Item>B</Item>
-      <Item>B</Item>
-      <Item>B</Item>
-      <Item>B</Item>
-      <Item>B</Item>
-      <Item>B</Item>
+      <Item>
+        <Link to="/new-video">
+          <Animation>
+            <FaPlus size={32} color={colors.white} />
+            <span>Novo video</span>
+            <Ink />
+          </Animation>
+        </Link>
+      </Item>
+
+      <Item>
+        <Link to="/new-meet">
+          <Animation>
+            <FaRegCalendarPlus size={32} color={colors.white} />
+            <span>Marcar reunião</span>
+            <Ink />
+          </Animation>
+        </Link>
+      </Item>
+
+      <Item>
+        <Link to="/new-report">
+          <Animation>
+            <FaRegPlayCircle size={32} color={colors.white} />
+            <span>Novo relatório</span>
+            <Ink />
+          </Animation>
+        </Link>
+      </Item>
+
+      <Item>
+        <Link to="/students">
+          <Animation>
+            <FaUsers size={32} color={colors.white} />
+            <span>Alunos</span>
+            <Ink />
+          </Animation>
+        </Link>
+      </Item>
+
+      <Item>
+        <Link to="/reports">
+          <Animation>
+            <FaArchive size={32} color={colors.white} />
+            <span>Relatórios</span>
+            <Ink />
+          </Animation>
+        </Link>
+      </Item>
+
+      <Item>
+        <Link to="/new-candidate">
+          <Animation>
+            <img src={advisorImg} alt="Conselheiros" />
+            <span>Tornar conselheiro</span>
+            <Ink />
+          </Animation>
+        </Link>
+      </Item>
     </Container>
   );
 };
--- src/components/AsideMenu/styles.ts
+++ src/components/AsideMenu/styles.ts
@@ -6,27 +6,105 @@ export const Container = styled.div`
   box-shadow: inset 0 0 1em transparent,
     0 0 0.4em ${props => props.theme.colors.primary};
 
-  width: 110px;
-  padding: 20px;
+  padding: 40px 20px;
 
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: flex-start;
 `;
 
 export const Item = styled.div`
-  background: ${props => props.theme.colors.primary};
-  border-radius: 50%;
+  position: relative;
 
   width: 58px;
   height: 58px;
 
+  & + div {
+    margin-top: 40px;
+  }
+
+  &:hover div {
+    border-radius: 38px 24px 48px 38px;
+    width: 186px;
+
+    box-shadow: inset 0 0 1em transparent,
+      0 0 0.4em ${props => props.theme.colors.primary};
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export const Animation = styled.div`
+  background: ${props => props.theme.colors.primary};
+  border-radius: 30px;
+
+  width: 60px;
+  height: 60px;
+
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 
-  & + div {
-    margin-top: 36px;
+  padding-left: 14px;
+
+  cursor: pointer;
+
+  position: absolute;
+
+  left: 0;
+
+  transition: width 200ms;
+
+  z-index: 2;
+
+  &:hover span {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  &:hover svg,
+  &:hover img {
+    animation: rotate 0.8s ease-in-out;
+    animation-timing-function: cubic-bezier(1, 0, 0.5, 1);
+  }
+
+  @keyframes rotate {
+    from {
+      -webkit-transform: rotate(0deg);
+    }
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+
+  img {
+    width: 32px;
+    height: 32px;
+  }
+
+  span {
+    position: absolute;
+    left: 64px;
+
+    max-width: 100px;
+
+    color: ${props =>
+      props.theme.title === 'light'
+        ? props.theme.colors.white
+        : props.theme.colors.black};
+    font-size: 16px;
+
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+
+    opacity: 0;
+    visibility: hidden;
+
+    transition: opacity 1s;
   }
 `;
