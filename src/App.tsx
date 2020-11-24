6b20c8b018c9cd3f6192f1a2f353f2409b0fff07
Bruno Futema <bruno.futema@outlook.com>
2020-11-23 23:56:53 -0300

    feat: :sparkles: Provide theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+
+import { HandlesProvider } from './context/handles';
+import usePersistedState from './hooks/usePersistedState';
 
 import './config/ReactotronConfig';
 
@@ -15,16 +18,26 @@ import dark from './styles/themes/dark';
 
 import { store, persistor } from './store';
 
-const App: React.FC = () => (
-  <ThemeProvider theme={dark}>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Routes />
-        <GlobalStyle />
-        <ToastContainer autoClose={3000} />
-      </PersistGate>
-    </Provider>
-  </ThemeProvider>
-);
+const App: React.FC = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+
+  const toggleTheme = useCallback(() => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }, [theme.title, setTheme]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <HandlesProvider toggleTheme={toggleTheme}>
+        <Provider store={store}>
+          <PersistGate persistor={persistor}>
+            <Routes />
+            <GlobalStyle />
+            <ToastContainer autoClose={3000} />
+          </PersistGate>
+        </Provider>
+      </HandlesProvider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
