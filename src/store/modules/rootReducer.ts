fa20b6ae5a50cedae2c24aece7e2b27591f418e1
Bruno Futema <bruno.futema@outlook.com>
2020-11-29 02:26:16 -0300

    feat: :sparkles: Connect new reducers to root reducer

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import auth from './auth/reducer';
 import user from './user/reducer';
 import video from './video/reducer';
+import meet from './meet/reducer';
 
 export default combineReducers({
   auth,
   user,
   video,
+  meet,
 });
