05da1be4c3b492fc0ce8b82b769d38668ef68901
Bruno Futema <bruno.futema@outlook.com>
2020-11-29 17:30:18 -0300

    feat: :sparkles: Connect report sagas to root saga

--- src/store/modules/rootSaga.ts
+++ src/store/modules/rootSaga.ts
@@ -4,7 +4,8 @@ import auth from './auth/sagas';
 import user from './user/sagas';
 import video from './video/sagas';
 import meet from './meet/sagas';
+import report from './report/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, user, video, meet]);
+  return yield all([auth, user, video, meet, report]);
 }
