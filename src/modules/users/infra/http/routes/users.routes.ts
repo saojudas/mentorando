3aa856b8b78f70198c489ad9c08276ae95da3371
Bruno Futema <bruno.futema@outlook.com>
2020-11-29 00:42:40 -0300

    feat: :sparkles: Add upload user avatar routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,17 +1,27 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import UsersController from '../controllers/UsersController';
+import UsersAvatarsController from '../controllers/UsersAvatarsController';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 const usersController = new UsersController();
+const usersAvatarsController = new UsersAvatarsController();
 
 usersRouter.post('/', usersController.create);
 
 usersRouter.use(ensureAuthenticated);
 
 usersRouter.get('/', usersController.index);
+usersRouter.patch(
+  '/avatar',
+  upload.single('avatar'),
+  usersAvatarsController.update,
+);
 usersRouter.get('/:id', usersController.show);
 
 export default usersRouter;
