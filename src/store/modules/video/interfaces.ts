483e2484f916e3baa3594f114f10a51e546f34d5
Bruno Futema <bruno.futema@outlook.com>
2020-11-29 02:22:37 -0300

    feat: :sparkles: Add thumbnail interface

--- src/store/modules/video/interfaces.ts
+++ src/store/modules/video/interfaces.ts
@@ -1,21 +1,32 @@
 export interface Thumbnail {
-  id: string;
-  name: string;
-  size: number;
-  key: string;
+  id?: string;
+  name?: string;
+  size?: number;
+  key?: string;
   url: string;
-  video_id: string;
-  created_at: Date;
-  updated_at: Date;
+  video_id?: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+export interface Tag {
+  id?: string;
+  name: string;
+  area_id?: string;
+  created_at?: Date;
+  updated_at?: Date;
 }
 
 export interface Video {
+  id: string;
+  key: string;
   title: string;
   thumbnail?: Thumbnail;
   video_link: string;
   description: string;
   area_id: string;
   tags_ids: string[];
+  tags: Tag[];
   created_at: Date;
   updated_at: Date;
 }
