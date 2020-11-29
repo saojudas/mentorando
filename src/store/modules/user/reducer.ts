bd691c63c92fb63d66b7bf145eafa2d6800197c3
Bruno Futema <bruno.futema@outlook.com>
2020-11-29 02:25:39 -0300

    feat: :sparkles: Add user avatar to user reducer

--- src/store/modules/user/reducer.ts
+++ src/store/modules/user/reducer.ts
@@ -2,14 +2,24 @@ import produce from 'immer';
 import { AnyAction } from 'redux';
 
 import { User } from '../auth/interfaces';
+
 import { SIGN_IN_SUCCESS } from '../auth/constants';
+import {
+  UPDATE_AVATAR_REQUEST,
+  UPDATE_AVATAR_SUCCESS,
+  UPDATE_AVATAR_FAILURE,
+} from './constants';
 
 interface AuthState {
   profile: User | undefined;
+  loading: boolean;
+  error: boolean;
 }
 
 const INITIAL_STATE = {
   profile: undefined,
+  loading: false,
+  error: false,
 };
 
 export default function user(
@@ -20,6 +30,24 @@ export default function user(
     switch (action.type) {
       case SIGN_IN_SUCCESS: {
         draft.profile = action.payload.user;
+        break;
+      }
+
+      case UPDATE_AVATAR_REQUEST: {
+        draft.loading = true;
+        break;
+      }
+
+      case UPDATE_AVATAR_SUCCESS: {
+        draft.loading = false;
+        if (draft.profile) draft.profile.avatar = action.payload.avatar;
+        draft.error = false;
+        break;
+      }
+
+      case UPDATE_AVATAR_FAILURE: {
+        draft.loading = false;
+        draft.error = true;
         break;
       }
 
