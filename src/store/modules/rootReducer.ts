44de8dc261f3990e8b922ce2dd07f24272d1b118
Bruno Futema <bruno.futema@outlook.com>
2020-11-29 17:29:38 -0300

    feat: :sparkles: Connect report reducer to root reducer

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -4,10 +4,12 @@ import auth from './auth/reducer';
 import user from './user/reducer';
 import video from './video/reducer';
 import meet from './meet/reducer';
+import report from './report/reducer';
 
 export default combineReducers({
   auth,
   user,
   video,
   meet,
+  report,
 });
