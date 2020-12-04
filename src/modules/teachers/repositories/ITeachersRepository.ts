eb7c58772df2c0e39c3582d02423b447150d4387
Bruno Futema <bruno.futema@outlook.com>
2020-12-04 11:45:32 -0300

    feat: :sparkles: Add findById to teachers repo

--- src/modules/teachers/repositories/ITeachersRepository.ts
+++ src/modules/teachers/repositories/ITeachersRepository.ts
@@ -5,6 +5,7 @@ import ICreateTeacherDTO from '../dtos/ICreateTeacherDTO';
 export default interface ITeachersRepository {
   create(data: ICreateTeacherDTO): Promise<Teacher>;
   save(user: Teacher): Promise<Teacher>;
+  findById(id: string): Promise<Teacher | undefined>;
   findByName(name: string): Promise<Teacher | undefined>;
   delete(id: string): Promise<void>;
 }
