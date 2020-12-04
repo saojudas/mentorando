af6b8831dcd16e8dd1c39026425efefb02a9a008
Bruno Futema <bruno.futema@outlook.com>
2020-12-04 14:54:14 -0300

    feat: :sparkles: Add video and area reference into user model

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -2,13 +2,17 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
   ManyToMany,
+  OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
+import Video from '../../../../videos/infra/typeorm/entities/Video';
 import Meet from '../../../../meet/infra/typeorm/entities/Meet';
+import Area from '../../../../teachers/infra/typeorm/entities/Area';
 import Teacher from '../../../../teachers/infra/typeorm/entities/Teacher';
 import Student from '../../../../students/infra/typeorm/entities/Student';
 import UserAvatar from './UserAvatar';
@@ -38,6 +42,16 @@ class User {
 
   @OneToOne(() => UserAvatar, userAvatar => userAvatar.user)
   avatar: UserAvatar;
+
+  @Column()
+  area_id: string;
+
+  @OneToOne(() => Area)
+  @JoinColumn({ name: 'area_id' })
+  area: Area;
+
+  @OneToMany(() => Video, video => video.user)
+  videos: Video[];
 
   @CreateDateColumn()
   created_at: Date;
