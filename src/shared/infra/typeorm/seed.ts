2b120bea4657e29e8cbdab88c2a0cf4778024b70
Bruno Futema <bruno.futema@outlook.com>
2020-12-31 01:25:07 -0300

    feat: :seedling: Add areas seeds

--- src/shared/infra/typeorm/seed.ts
+++ src/shared/infra/typeorm/seed.ts
@@ -1,7 +1,9 @@
 import { usersSeed } from './seeds/users.seed';
+import { areasSeed } from './seeds/areas.seed';
 
 async function init(): Promise<void> {
   await usersSeed();
+  await areasSeed();
 }
 
 init();
