d2cd69ac101cc916c0b4483642a1fc9bd05b4258
Bruno Futema <bruno.futema@outlook.com>
2020-12-31 03:09:43 -0300

    feat: :seedling: Add videos seeds

--- src/shared/infra/typeorm/seed.ts
+++ src/shared/infra/typeorm/seed.ts
@@ -1,11 +1,14 @@
 import { usersSeed } from './seeds/users.seed';
 import { areasSeed } from './seeds/areas.seed';
 import { tagsSeed } from './seeds/tags.seed';
+import { videosSeed } from './seeds/videos.seed';
 
 async function init(): Promise<void> {
-  await usersSeed();
+  const usersResponse = await usersSeed();
   const areasResponse = await areasSeed();
   await tagsSeed(areasResponse.areasIds);
+
+  await videosSeed(areasResponse.areasIds, usersResponse.usersIds);
 }
 
 init();
