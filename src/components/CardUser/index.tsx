59fad41ff56eee8f77dd1029383c3f198720782f
Bruno Futema <bruno.futema@outlook.com>
2020-12-31 19:39:00 -0300

    feat: :sparkles: Adjust in the logic of rendering the advisor seal

--- src/components/CardUser/index.tsx
+++ src/components/CardUser/index.tsx
@@ -9,14 +9,13 @@ interface CardUser {
   name: string;
   area?: string;
   avatar?: string;
+  isAdvisor?: boolean;
 }
 
-const CardUser: React.FC<CardUser> = ({ name, area, avatar }) => {
-  const adviser = true;
-
+const CardUser: React.FC<CardUser> = ({ name, area, avatar, isAdvisor }) => {
   return (
     <Container>
-      {adviser && <AdviserSVG />}
+      {isAdvisor && <AdviserSVG />}
 
       <CardBody>
         <img src={avatar || defaultAvatar} alt={`Foto de ${name}`} />
